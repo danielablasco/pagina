360f69c363de52da23e1245a2a20ca8f3f6ce633
danielablasco <131489146+danielablasco@users.noreply.github.com>
2023-10-25 00:34:26 -0300

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,153 @@
+const data = {
+    status: "success",
+    items: [
+      {
+        id: 1,
+        title: "Conjunto Antonia",
+        price: 2200,
+        image: require('./images/antonianegro.jpg'),
+        category: "Encaje",
+        cantidad: 1,
+      },
+      {
+        id: 2,
+        title: "Emily Bordo",
+        price: 2600,
+        image: require('./images/EmilyBordo.jpg'),
+        category: "Triangulito",
+        cantidad: 1
+      },
+      {
+        id: 3,
+        title: "Lisboa Negro",
+        price: 475,
+        image: require('./images/LisboaNegro.jpg'),
+        category: "SinArmar",
+        cantidad: 1
+      },
+      {
+        id: 4,
+        title: "Olivia Rosa",
+        price: 2600,
+        image: require('./images/OliviaRosa.jpg'),
+        category: "Triangulito",
+        cantidad: 1
+      },
+      {
+        id: 5,
+        title: "Olivia Rojo",
+        price: 2600,
+        image: require('./images/OliviaRojo.jpg'),
+        category: "Triangulito",
+        cantidad: 1
+      },
+      {
+        id: 6,
+        title: "Emily Negro",
+        price: 2600,
+        image: require('./images/emilyNegro.jpg'),
+        category: "Triangulito",
+        cantidad: 1
+      },
+      {
+        id: 7,
+        title: "Hailey Negro",
+        price: 2500,
+        image: require('./images/HaileyNegro.jpg'),
+        category: "Armados",
+        cantidad: 1
+      },
+      {
+        id: 8,
+        title: "Hailey Verde",
+        price: 2500,
+        image: require('./images/HaileyVerde.jpg'),
+        category: "Armados",
+        cantidad: 1
+      },
+      {
+        id: 9,
+        title: "Margot Rojo",
+        price: 2600,
+        image: require('./images/MargotRojo.jpg'),
+        category: "Armados",
+        cantidad: 1
+      },
+      {
+        id: 10,
+        title: "Pijama",
+        price: 4600,
+        image: require('./images/pijama1.jpg'),
+        category: "Pijama",
+        cantidad: 1
+      },
+      {
+        id: 11,
+        title: "Pijama",
+        price: 4600,
+        image: require('./images/pijama2.jpg'),
+        category: "Pijama",
+        cantidad: 1
+      },
+      {
+        id: 12,
+        title: "Pijama",
+        price: 4600,
+        image: require('./images/pijama3.jpg'),
+        category: "Pijama",
+        cantidad: 1
+      },
+      {
+        id: 13,
+        title: "Bombis x2",
+        price: 1250,
+        image: require('./images/bombis1.jpg'),
+        category: "Bombis",
+        cantidad: 1
+      },
+      {
+        id: 14,
+        title: "Bombis x2",
+        price: 1250,
+        image: require('./images/bombis2.jpg'),
+        category: "Bombis",
+        cantidad: 1
+      },
+      {
+        id: 15,
+        title: "Bombis x2",
+        price: 1250,
+        image: require('./images/bombis3.jpg'),
+        category: "Bombis",
+        cantidad: 1
+      },
+      {
+        id: 16,
+        title: "Ligas Cruz",
+        price: 1950,
+        image: require('./images/ligasdobletira.jpg'),
+        category: "Arneses",
+        cantidad: 1
+      },
+      {
+        id: 17,
+        title: "Medias Glow!",
+        price: 2100,
+        image: require('./images/mediasglitter.jpg'),
+        category: "Medias",
+        img1: "https://http2.mlstatic.com/D_NQ_NP_739640-MLA32017224271_082019-O.webp",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 18,
+        title: "Alexia Rosa",
+        price: 1700,
+        image: require('./images/alexiaRosa.jpg'),
+        category: "SinArmar",
+        cantidad: 1
+      },
+    ],
+  };
+  
+  export default data;
